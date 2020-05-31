82cac785218d79f1f6f36e7fae45687b111b0296
Sandeep Kaushik <sandeep.kaushik@qlik.com>
2020-05-31 10:45:42 +0200

    changed write to file

--- test/firstTest.spec.js
+++ test/firstTest.spec.js
@@ -1,32 +1,27 @@
 const EC = protractor.ExpectedConditions;
 const fs = require('fs');
-XLSX = require('xlsx');
+
 const pageObject = require('../pageObjects/utils.js');
 
 describe('Doctrine tests', () => {
-  
-    it('Open URL and save details to a file', async() => {
-      const page = new pageObject();
-      browser.ignoreSynchronization = true;    
-      await page.get('https://career.doctrin.se/people');
-    await browser.wait(EC.visibilityOf(element(By.className('name'))), 5000,'Page not loaded');
-  var foo = element(By.xpath('//*[@id="top"]/section[3]/div/div/ul/li[1]/a/span[1]')).getText().then((test) => {
- console.log(test);
- expect(test).toEqual('Anders Fjeldstad');
- 
-let txtFile = "test.txt";
-fs.writeFileSync(txtFile, test);
-   } );
 
+  it('Open URL and save details to a file', async () => {
+    debugger;
+    const page = new pageObject();
+    browser.ignoreSynchronization = true;
+    await page.get('https://career.doctrin.se/people');
+    await browser.wait(EC.visibilityOf(element(By.className('name'))), 5000, 'Page not loaded');
 
+    element.all(By.className('section-padding employees-list')).getText().then((test) => {
 
- //// let txtFile = "test.txt";
-//fs.writeFileSync(txtFile, foo);
-    // expect(foo.getText()).toEqual('Afnders Fjeldstad');
-     
-  
-      
+      let txtFile = "test.json";
+      //let jsonStr = JSON.parse('{test}');
+      fs.writeFileSync(txtFile, test);
+
     });
-    
+
+
+  });
+
 
 });
